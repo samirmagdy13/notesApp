95c0696c364f767c7c7bdd0248cbc636f4e594c3
samir magdy <smagdey58@gmail.com>
2023-09-19 00:45:54 +0300

    add splash screen

---
+++ lib/Features/Splash/presentation/view/widgets/splash_view_body.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/Features/Splash/presentation/view/widgets/sliding_text.dart';
+
+import '../../../../home/presentation/view/home_view.dart';
+import 'animation_text.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(height: 30),
+        const AnimationText(),
+        const SizedBox(height: 15),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+          return const NoteView();
+        }));
+      },
+    );
+  }
+}
